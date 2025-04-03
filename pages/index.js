8016fc59408643925ac44f88fd3f4dcc8d4e65e3
Henrique Silva <henriquecontato746@gmail.com>
2025-04-03 02:36:24 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>Espero que eu traga orgulho para você, eu vou trabalhar muito para isso se realizar!!</h1>
+function Home() {
+  return (
+    <h1>
+      Espero que eu traga orgulho para você, eu vou trabalhar muito para isso se
+      realizar!!
+    </h1>
+  );
 }
 
 export default Home;
